a1b3e798804decd64f17d96dcf97cb686c75d25c
jjeyo <gatestsummer1@gmail.com>
2020-03-03 18:42:58 +0900

    추가 숙제_야구게임_중복되지 않은 숫자로 수정한 버전

--- hw0226.py
+++ hw0226.py
@@ -5,17 +5,18 @@ ans = ["숙제가 참 쉽죠?! 와~신난다...",
        "누가 챗봇소리를 내었는가",
        "코로나바이러스때문에 재택근무 하고싶어요",
        "그냥 재택근무가 하고 싶은 베짱이"]
-finish = ["Quit", "quit", "Exit", "QUIT", "exit", "EXIT"]
+finish = ["quit", "exit"]
 print("반갑습니다. 최첨단 인공지능 대화로봇을 지향만합니다.")
 
 while True:
     choice = random.randint(0, 3)
     answer = ans[int(choice)]
     a = input("아무거나 물어보세요~\n>")
+    a.strip()
     # 'input'에 공백이 들어왔을 때, 제거는?
-    if a in finish:
+    if a.strip().lower() in finish:
         break
-    elif a == '':
+    elif a.strip() == '':
         print("뭐라도 말을 해야 답인지 알려주지.. 다시해요")
     else:
         print("제 대답은요..", end="")
@@ -28,4 +29,6 @@ while True:
         print(answer)
 
 input("엔터치면 마칩니다.")
+
+
 
--- hw0304_add.py
+++ hw0304_add.py
@@ -1,36 +1,37 @@
 import random
 
+# player1이 중복되지 않은 1~9까지의 숫자를 3개 고르기
+pick_num = [random.randint(1, 9), random.randint(1, 9), random.randint(1, 9)]
+while len(set(pick_num)) != 3:
+    pick_num = [random.randint(1, 9), random.randint(1, 9), random.randint(1, 9)]
 
-pick_num = random.randint(100, 999)
-count_s = 0
-count_b = 0
-
+# player2의 입력값을 받아서 검토하기
 while True:
     while True:
         try:
-            user_num = int(input('야구게임입니다. 100 부터 999 사이의 3자리 숫자를 입력해주세요.\n>'))
+            user_num = int(input('야구게임입니다. 0을 제외한 3자리 숫자를 입력해주세요.\n>'))
             if 100 < (user_num % 1000) < 1000:
-                break
-            elif (user_num % 1000) < 100:
-                print('100에서 999사이의 3자리 숫자를 입력해주세요.\n')
+                user_num = list(str(user_num))
+                if user_num[1] != '0' and user_num[2] != '0':
+                    break
         except ValueError:
-            print('문자가 아닌 숫자만 입력해주세요.\n')
             continue
 
-    nl_1 = str(pick_num)
-    nl_2 = str(user_num)
+# player2의 올바른 입력값을 player1의 랜덤 숫자와 비교하여 야구게임하기
+    count_s = 0
+    count_b = 0
+
     for i in range(0, 3):
-        if nl_2[i] == nl_1[i]:
-            count_s = count_s + 1
-        elif nl_2.find(nl_1[i]) != -1:
-            count_b = count_b + 1
-        else:
-            continue
+        for k in range(0, 3):
+            if i == k and str(pick_num[i]) == user_num[k]:
+                count_s = count_s + 1
+            elif i != k and str(pick_num[i]) == user_num[k]:
+                count_b = count_b + 1
     if count_s == 3:
         break
-    else:
-        print("%s, %s strike, %s ball" % (pick_num, count_s, count_b))
-        count_s = 0
-        count_b = 0
+    print("%s strike, %s ball" % (count_s, count_b))
 
 print('Home Run!')
+
+
+
